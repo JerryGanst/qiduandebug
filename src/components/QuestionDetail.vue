027c3b1e4e646ef321ff3f4e5debd907db23e15f
T10802004 <Yiqian.Yuan@luxshare-ict.com>
2025-03-05 08:56:46 +0800

    首次提交平台AI代码

---
+++ src/components/QuestionDetail.vue
@@ -0,0 +1,60 @@
+<template>
+    <el-container>
+      <el-header>
+        <el-button type="text" @click="goBack">返回</el-button>
+        <h2>{{ question.title }}</h2>
+      </el-header>
+      <el-main>
+        <el-card>
+          <div>{{ question.content }}</div>
+          <div>提问者: {{ question.author }}</div>
+          <div>日期: {{ question.date }}</div>
+        </el-card>
+        <el-divider></el-divider>
+        <h3>回答</h3>
+        <el-input
+          type="textarea"
+          :rows="4"
+          placeholder="请输入你的回答"
+          v-model="answer"
+        ></el-input>
+        <el-button type="primary" @click="submitAnswer">提交回答</el-button>
+      </el-main>
+    </el-container>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      question: {
+        type: Object,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        answer: '',
+      };
+    },
+    methods: {
+      goBack() {
+        this.$emit('go-back');
+      },
+      submitAnswer() {
+        if (this.answer.trim()) {
+          this.$emit('submit-answer', this.answer);
+          this.answer = '';
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .el-header {
+    background-color: #409EFF;
+    color: white;
+    text-align: center;
+    line-height: 60px;
+  }
+  </style>
---
+++ src/utils/request.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+// 创建 axios 实例
+const service = axios.create({
+  baseURL: 'http://121.201.35.34:11234', // 服务器地址
+  timeout: 10000, // 请求超时时间
+});
+
+// 请求拦截器
+service.interceptors.request.use(
+  (config) => {
+    // 设置请求头
+    config.headers['Content-Type'] = 'application/json';
+    return config;
+  },
+  (error) => {
+    // 请求错误处理
+    return Promise.reject(error);
+  }
+);
+
+// 响应拦截器
+service.interceptors.response.use(
+  (response) => {
+    // 对响应数据做处理
+    return response.data;
+  },
+  (error) => {
+    // 响应错误处理
+    return Promise.reject(error);
+  }
+);
+
+// 封装通用请求方法
+export const postRequest = (url, data) => {
+  return service({
+    method: 'post',
+    url,
+    data: JSON.stringify(data), // 将参数转换为 JSON 字符串
+  });
+};
+
+export default service;
