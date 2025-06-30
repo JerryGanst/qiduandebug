62cacd0603472ab18443d4b1473fc5a09254dca3
T10802004 <Yiqian.Yuan@luxshare-ict.com>
2025-06-30 15:25:04 +0800

    遮罩

--- src/pages/main/component/personModal.vue
+++ src/pages/main/component/personModal.vue
@@ -757,10 +757,10 @@ defineExpose({ openFile })
 <style scoped lang="less">
 .loading-mask {
   position: absolute;
-  top: 0;
+  top: 100px;
   left: 0;
   right: 0;
-  bottom: 0;
+  bottom: 140px;
   background-color: transparent;
   display: flex;
   justify-content: center;
